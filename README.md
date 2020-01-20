26572e610270b9780a40b2ee4fbd2314f51b4469
rphanley <rphanley@rocketmail.com>
2020-01-20 14:55:35 +0000

    Initial commit. Adds 'galore' and accounts apps, manage.py, base.html and accounts templates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Code Institute
 
-Welcome USER_NAME,
+Welcome rphanley,
 
 We have preinstalled all of the tools you need to get started.
 
---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from accounts import urls_reset
+from .views import index, register, profile, logout, login
+
+urlpatterns = [
+    url(r'^$', index, name="index"),
+    url(r'^register/$', register, name='register'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^login/$', login, name='login'),
+    url(r'^password-reset/', include(urls_reset)),
+]
