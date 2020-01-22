c477d9e2cabf42bac21423040f15558be60d93b2
rphanley <rphanley@rocketmail.com>
2020-01-22 13:29:26 +0000

    Adds allowed host for Heroku

--- galore/settings.py
+++ galore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'shopping-galore.herokuapp.com']
 
 
 # Application definition
